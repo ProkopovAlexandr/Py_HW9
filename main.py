d168786f3b02b11270fc3be681f6220700586539
ProkopovAlexandr <119793734+ProkopovAlexandr@users.noreply.github.com>
2023-04-28 20:50:56 +0300

    Add files via upload
    Выполненное ДЗ

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import csv
 import pandas as pd
 
 csv_file = pd.read_csv('sample_data/california_housing_test.csv')
-print(csv_file['median_house_value'].mean())
+print(csv_file[csv_file['population'] <= 500] ['median_house_value'])
 
+print(f"Mаксимальная households в зоне минимального значения population равна: {csv_file[csv_file['population'] == csv_file['population'].min()] ['households'].min()}")
